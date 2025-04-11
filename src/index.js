a17025e8b4f7d2d3b82d3b80d865d5a58ed7dc0c
avilesha <avileshagovender95@gmail.com>
2025-04-11 20:44:49 +0200

    made corrections to JavaScript and html

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ new Typewriter("#poem", {
 function generatePoem(event) {
     event.preventDefault()
 
-  let instructonsInput = document.querySelector("user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "f3a1ab1884f405093fec0555dtb0oa41";
 let prompt = ` User instructions: Generate a English poem about ${instructionsInput.value}`;
 let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
