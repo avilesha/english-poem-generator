b7e0e7287f2772f372d354d4f462704f7cac3339
avilesha <avileshagovender95@gmail.com>
2025-04-14 11:52:36 +0200

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -16,8 +16,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "f3a1ab1884f405093fec0555dtb0oa41";
 let prompt = ` User instructions: Generate a English poem about ${instructionsInput.value}`;
-let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+let context = 
+"You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating"⏳Generating a English Poem about ${instructionsInput.value}</div>`;
 
 console.log(`Prompt: ${prompt}`);
 console.log(`Context: ${context}`);
